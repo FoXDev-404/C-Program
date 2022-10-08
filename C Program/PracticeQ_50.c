cc2c0d4dd37e932c64dd4653116b8a015a137eca
Rajpal Nishad <rajpal010304@gmail.com>
2022-10-08 22:51:46 +0530

    Take a string input from the user using %c

---
+++ C Program/PracticeQ_50.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+void printString(char arr[]);
+void countLength(char arr[]);
+
+/* Take a string input from the user using %c */
+
+int main()
+{
+    char str[100];
+    char ch;
+    int i = 0;
+
+    while (ch != '\n')
+    {
+        scanf("%c", &ch); /* This is also count space(_) */
+        str[i] = ch;
+        i++;
+    }
+    str[i] = '\0';
+    puts(str);
+}
